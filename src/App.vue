28287cbb1fbe826e8b557a6adcc301f8dad45ec1
brumePyvault <oghenebrume.akpadaka@gmail.com>
2025-09-27 17:39:11 +0100

    feat: initialize Vue 3 project with Vite, Pinia, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, building, and testing.
    - Created favicon.ico for the application.
    - Implemented main App.vue component with ChatboxComp.
    - Added unit tests for App.vue using Vitest.
    - Developed ChatboxComp component for chat interface.
    - Created ChatboxInputComp component as a placeholder for input.
    - Set up Tailwind CSS in index.css.
    - Configured main.ts to initialize Vue app with Pinia and router.
    - Established router in src/router/index.ts.
    - Created a counter store using Pinia in src/stores/counter.ts.
    - Configured TypeScript settings in tsconfig files for app and node.
    - Set up Vite configuration in vite.config.ts with Vue and Tailwind plugins.
    - Configured Vitest for testing environment in vitest.config.ts.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ChatboxComp from './components/ChatboxComp.vue'
+
+let apiUrl = import.meta.env.VITE_SERVER_URL
+</script>
+
+<template>
+  <section class="bg-[#69c3e633] h-dvh w-dvw overflow-hidden">
+    <div class="mx-auto w-fit h-full py-20">
+      <section class="mx-auto w-fit h-[80%]">
+        <ChatboxComp />
+      </section>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ src/components/ChatboxComp.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section
+    class="h-full max-w-full border-4 w-[600px] rounded-2xl bg-white border-[#6fa2e6] overflow-y-auto"
+  ></section>
+</template>
